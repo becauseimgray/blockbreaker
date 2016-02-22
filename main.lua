9e50d6a937f6d84bcff7bbf3d646fa9e5ced183c
becauseimgray <otakugray@gmail.com>
2016-02-21 18:41:42 -0700

    Made gamestates, added ball going off-screen, added fonts

--- main.lua
+++ main.lua
@@ -6,7 +6,6 @@ local ball = {}
 local screen = {}
 function love.load()
 
-
   screen.width = 1024
   screen.height = 768
 
@@ -15,8 +14,6 @@ function love.load()
   rect1.width = 100
   rect1.height = 10
   rect1.speed = 1000
-  rect1.friction = 5
-
 
   ball.x = screen.width / 2
   ball.y = screen.height / 2
@@ -25,6 +22,8 @@ function love.load()
   ball.speed = 500
   ball.speedx = -ball.speed
   ball.speedy = ball.speed
+  ball.velocity = ball.speed + rect1.speed
+  start = true
 end
 
 -- Creating Collision Function for later usage
@@ -36,8 +35,13 @@ function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
 end
 
 function love.update(dt)
-
-  -- controls for dock
+  if love.keyboard.isDown('space') then
+    start = false
+  end
+  if love.keyboard.isDown('escape') then
+    start = true
+  end
+    -- controls for dock
   if love.keyboard.isDown('d') then
     rect1.x = rect1.x + (rect1.speed * dt)
   end
@@ -60,14 +64,14 @@ function love.update(dt)
     rect1.y = screen.height - rect1.height
   end
 
-  --literally stole this from pong
   ball.x = ball.x + (ball.speedx * dt)
   ball.y = ball.y + (ball.speedy * dt)
 
   if ball.y < 0 then
       ball.speedy = math.abs(ball.speedy)
   elseif (ball.y + ball.h) > screen.height then
-     ball.speedy = -math.abs(ball.speedy)
+    ball.speedy = -math.abs(ball.speedy)
+    start = true
   end
   if ball.x < 0 then
       ball.speedx = math.abs(ball.speedx)
@@ -79,15 +83,20 @@ function love.update(dt)
   if CheckCollision(rect1.x,rect1.y,rect1.width,rect1.height,
   ball.x, ball.y, ball.w, ball.h) then
   ball.speedy = -math.abs(ball.speedy)
-  ball.speed = ball.speed + 10
+  ball.speed = ball.velocity + 10
   end
-
-
-
-
 end
 
 function love.draw()
+  if start then
+    ball.x = screen.width / 2, screen.height / 2
+    ball.y = screen.width / 2, screen.height / 2
+
+    font = love.graphics.newFont("fonts/Vera.ttf", 50)
+    love.graphics.setFont(font)
+    love.graphics.print("Press Space", screen.width / 2 - 150, screen.height / 2 - 100)
+  end
+
   love.graphics.rectangle('fill', rect1.x, rect1.y, rect1.width,rect1.height)
   love.graphics.circle('fill', ball.x,ball.y, ball.h, ball.w)
 end
