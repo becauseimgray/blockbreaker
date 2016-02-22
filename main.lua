ab0cdf089037e01f63c1f80bdb1753866643e0d4
becauseimgray <otakugray@gmail.com>
2016-02-22 03:59:40 -0700

    Cleaned up some of the code, removed an image I wasn't using, changed the resolution, added more blocks, and worked a bit more on android support.

--- main.lua
+++ main.lua
@@ -13,7 +13,7 @@ local screen = {}
 
 function love.load()
   --load android functions
-  pGame = gooi.newPanel("panelGameLayout", 20, 500, 200, 200, "game")
+  pGame = gooi.newPanel("panelGameLayout", 0, 800, 200, 200, "game") -- this changes gui location
   pGame:add(gooi.newJoy("joy_1"), "b-l")-- Bottom-left
 
   screen.width, screen.height, screen.flags = love.window.getMode()
@@ -38,8 +38,11 @@ function love.load()
   ball.speedx = -ball.speed
   ball.speedy = ball.speed
   ball.velocity = ball.speed + rect1.speed
+
+  startmsg = love.graphics.newImage("images/startmsg.png")
   start = true
   alpha = 255
+  --sounds
   list = {
     "audio/sound1.wav",
     "audio/sound2.wav",
@@ -52,7 +55,7 @@ function love.load()
     "audio/sound9.wav",
     "audio/sound10.wav",
   } --loads sounds and puts them into a list for TEsound work with
-      map1.load()
+      map1.load() --level loading
 end
 
 -- Creating Collision Function for later usage
@@ -67,6 +70,7 @@ function love.update(dt)
   TEsound.cleanup()
   gooi.update(dt)
 
+  --keyboard controls for beginning game and pausing
   if love.keyboard.isDown('space') then
     start = false
   end
@@ -100,7 +104,7 @@ function love.update(dt)
   if rect1.y > screen.height then
     rect1.y = screen.height - rect1.height
   end
-
+  --ball movement
   ball.x = ball.x + (ball.speedx * dt)
   ball.y = ball.y + (ball.speedy * dt)
 
@@ -109,8 +113,7 @@ function love.update(dt)
   elseif (ball.y + ball.h) > screen.height then
     ball.speedy = -math.abs(ball.speedy)
     start = true
-    block1.active = true
-    block2.active = true
+    map1.reset(true)
     if points >= highscore then
       highscore = points
     end
@@ -122,7 +125,7 @@ function love.update(dt)
   elseif (ball.x + ball.w) > screen.width  then
       ball.speedx = -math.abs(ball.speedx)
   end
-
+  --updates level
   map1.update(dt, ball)
 
   if CheckCollision(rect1.x,rect1.y,rect1.width,rect1.height,
@@ -145,25 +148,33 @@ function love.draw()
   gooi.draw()
 
   --love.graphics.setColor(0, 0, 0, 255)
+
   titlebar.bar = love.graphics.rectangle("line", titlebar.x, titlebar.y,titlebar.w, titlebar.h)
   love.graphics.print(points, 10, 10)
   love.graphics.print("HS:" .. highscore, screen.width - 300, 10)
   love.graphics.setColor(255, 255, 255, 255)
+
   if start then
+    --ball starting location
     ball.x = screen.width / 2, screen.height / 2
     ball.y = screen.width / 2, screen.height / 2
+    --font setup
     love.graphics.setColor(255,255,255, alpha)
     font = love.graphics.newFont("fonts/Vera.ttf", 50)
     love.graphics.setFont(font)
-    love.graphics.print("Press Space", screen.width / 2 - 150, screen.height / 2 - 100)
+    --Starting message
+    love.graphics.draw(startmsg, 0, 0)
   end
 
     love.graphics.rectangle('fill', rect1.x, rect1.y, rect1.width,rect1.height)
+    --draws level
         map1.draw()
   if start == false then
     love.graphics.circle('fill', ball.x,ball.y, ball.h, ball.w)
   end
 
+
+--extras for touch
 function love.mousereleased(x, y, button) gooi.released() end
 function love.mousepressed(x, y, button)  gooi.pressed() start = false end
 
@@ -172,8 +183,5 @@ function love.mousepressed(x, y, button)  gooi.pressed() start = false end
 function love.touchmoved(id, x, y, pressure) gooi.moved(id, x, y) start = false end
 function love.touchpressed(id, x, y, pressure) gooi.pressed(id, x, y) end
 function love.touchreleased(id, x, y, pressure) gooi.released(id, x, y) end
-
-
-
 
 end
--- map1.lua
+++ map1.lua
@@ -1,45 +1,63 @@
 require "lick"
-block1 = {}
-block2 = {}
 map1 = {}
 
+
 function map1.load()
-block2.x = 165
-block2.y = 80
-block2.w = 164
-block2.h = 64
-
-block1.x = 0
-block1.y = 80
-block1.w = 164
-block1.h = 64
-
-block2.active = true
-block1.active = true
+  block1 = {x = 0,              y = 80, w = 164, h = 64, active = true}
+  block2 = {x = block1.x + 165, y = 80, w = 164, h = 64, active = true}
+  block3 = {x = block2.x + 165, y = 80, w = 164, h = 64, active = true}
+  block4 = {x = block3.x + 165, y = 80, w = 164, h = 64, active = true}
+  block5 = {x = block4.x + 165, y = 80, w = 164, h = 64, active = true}
+  block6 = {x = block5.x + 165, y = 80, w = 164, h = 64, active = true}
+  --block7 = {x = block6.x + 165, y = 80, w = 164, h = 64, active = true}
 end
 
 function map1.update(dt, ball)
-  if block1.active and CheckCollision(block1.x,block1.y,block1.w,block1.h,
-    ball.x, ball.y, ball.w, ball.h) then
-    ball.speedy = math.abs(ball.speedy)
-    TEsound.play(list)
-        points = points + 1
+
+  if block1.active and CheckCollision(block1.x,block1.y,block1.w,block1.h, ball.x, ball.y, ball.w, ball.h) then
+    ball.speedy = math.abs(ball.speedy) TEsound.play(list) points = points + 1
     block1.active = false
   end
-  if block2.active and CheckCollision(block2.x,block2.y,block2.w,block2.h,
-    ball.x, ball.y, ball.w, ball.h) then
-    ball.speedy = math.abs(ball.speedy)
-    TEsound.play(list)
+
+  if block2.active and CheckCollision(block2.x,block2.y,block2.w,block2.h, ball.x, ball.y, ball.w, ball.h) then
+    ball.speedy = math.abs(ball.speedy) TEsound.play(list) points = points + 1
     block2.active = false
-    points = points + 1
   end
+
+  if block3.active and CheckCollision(block3.x,block3.y,block3.w,block3.h, ball.x, ball.y, ball.w, ball.h) then
+    ball.speedy = math.abs(ball.speedy) TEsound.play(list) points = points + 1
+    block3.active = false
+  end
+  if block4.active and CheckCollision(block4.x,block4.y,block4.w,block4.h, ball.x, ball.y, ball.w, ball.h) then
+    ball.speedy = math.abs(ball.speedy) TEsound.play(list) points = points + 1
+    block4.active = false
+  end
+  if block5.active and CheckCollision(block5.x,block5.y,block5.w,block5.h, ball.x, ball.y, ball.w, ball.h) then
+    ball.speedy = math.abs(ball.speedy) TEsound.play(list) points = points + 1
+    block5.active = false
+  end
+  if block6.active and CheckCollision(block6.x,block6.y,block6.w,block6.h, ball.x, ball.y, ball.w, ball.h) then
+    ball.speedy = math.abs(ball.speedy) TEsound.play(list) points = points + 1
+    block6.active = false
+  end
+
 end
 
 function map1.draw()
-  if block1.active then
-    love.graphics.rectangle("fill",block1.x, block1.y, block1.w, block1.h)
-  end
-  if block2.active then
-    love.graphics.rectangle("fill",block2.x, block2.y, block2.w, block2.h)
-  end
+  if block1.active then love.graphics.rectangle("fill",block1.x, block1.y, block1.w, block1.h) end
+  if block2.active then love.graphics.rectangle("fill",block2.x, block2.y, block2.w, block2.h) end
+  if block3.active then love.graphics.rectangle("fill",block3.x, block3.y, block3.w, block3.h) end
+  if block4.active then love.graphics.rectangle("fill",block4.x, block4.y, block4.w, block4.h) end
+  if block5.active then love.graphics.rectangle("fill",block5.x, block5.y, block5.w, block5.h) end
+  if block6.active then love.graphics.rectangle("fill",block6.x, block6.y, block6.w, block6.h) end
+  --if block7.active then love.graphics.rectangle("fill",block7.x, block7.y, block7.w, block7.h) end
+end
+
+function map1.reset(bool)
+  block1.active = bool
+  block2.active = bool
+  block3.active = bool
+  block4.active = bool
+  block5.active = bool
+  block6.active = bool
 end
